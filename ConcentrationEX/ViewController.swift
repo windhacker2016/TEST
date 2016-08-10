605307fe29b00cec10e3533671c0c472e2db4e9d
Alexander <Alexander@wangrenpeide-macbook-air.local>
2016-08-10 13:36:21 +0800

    Initial Commit

---
+++ ConcentrationEX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConcentrationEX
+//
+//  Created by Alexander on 2016/8/10.
+//  Copyright © 2016年 Make Sachool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
