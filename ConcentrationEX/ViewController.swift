bfa756854ac8436f3cb03ecd2353b7c4ac08bec4
Alexander <Alexander@wangrenpeide-macbook-air.local>
2016-08-18 17:35:12 +0800

    Back to work

--- ConcentrationEX/ViewController.swift
+++ ConcentrationEX/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // 分子量查詢輸入格
     @IBOutlet weak var formulaTextField: UITextField!
+  
+    // 莫耳濃度公式輸入格
+    @IBOutlet weak var pickMassTextField: UITextField!
     
+    @IBOutlet weak var pickMWTextField: UITextField!
+
+    @IBOutlet weak var pickVolumeTextField: UITextField!
+    
+    // 濃度換算公式輸入格
     @IBOutlet weak var m1TextField: UITextField!
 
     @IBOutlet weak var v1TextField: UITextField!
@@ -20,6 +29,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var v2TextField: UITextField!
     
+    
+    // 週期表Dictionary
     var periodicTable = ["H":1.008,"He":4.00260,"Li":6.94,"Be":9.01218,"B":10.81,"C":12.011,"N":14.007,"O":15.999,"F":18.9984,"Ne":20.1797,"Na":22.9897,"Mg":24.305,"Al":26.9815]
     
     func findMolecularWeight(molecular:String?) {
@@ -29,7 +40,8 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func formula(sender: AnyObject){
+    // 分子量查詢功能
+    @IBAction func calculateMW(sender: AnyObject){
         
         if formulaTextField == nil {
             return
@@ -39,8 +51,51 @@ class ViewController: UIViewController {
     }
 
     
-
-    @IBAction func caculate(sender: AnyObject)
+    // 莫耳濃度計算功能
+    @IBAction func calculateM(sender: AnyObject) {
+        
+        var mass:Double? = Double(pickMassTextField.text!)
+        var mw:Double? = Double(pickMWTextField.text!)
+        var vol:Double? = Double(pickVolumeTextField.text!)
+        
+        // 避免未正確輸入崩潰
+        func showError() -> Int {
+        
+            var count:Int = 0
+            
+            if mass != nil{
+                count+=1
+            }
+            if mw != nil{
+                count+=1
+            }
+            if vol != nil{
+                count+=1
+            }
+            return count
+        }
+        let counter = showError()
+        
+        if counter < 2 {return}
+        
+        //公式
+        if mass == nil {
+            mass = mw! * vol!
+            pickMassTextField.text = "\(mass)"
+        }
+        if formulaTextField.text == nil {
+            mw = formulaTextField
+            pickMassTextField.text = "\(mass)"
+        }
+        if mass == nil {
+            mass = mw! * vol!
+            pickMassTextField.text = "\(mass)"
+        }
+    }
+    
+    
+    //濃度換算計算功能
+    @IBAction func caculateC(sender: AnyObject)
      {
         
         var m1:Double? = Double(m1TextField.text!)
@@ -48,6 +103,7 @@ class ViewController: UIViewController {
         var m2:Double? = Double(m2TextField.text!)
         var v2:Double? = Double(v2TextField.text!)
         
+        // 避免未正確輸入崩潰
         func showError() -> Int {
             
             var count:Int = 0
@@ -72,6 +128,7 @@ class ViewController: UIViewController {
         
         if counter < 3 {return}
         
+        // 公式
         if v2 == nil {
             v2 = m1! * v1! / m2!
             v2TextField.text = "\(v2!)"
